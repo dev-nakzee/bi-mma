51d72e0e6d4864fa73e1793331e0b017128a2561
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-10-12 13:05:17 +0530

    Update home.blade.php

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -14,7 +14,7 @@
             @endif
         </ul>
     </div>
-    <div class="section-2 text-center p-5 my-3 bg-grey-light text-grey">
+    <div class="section-2 text-center p-5 my-3 bg-orange text-grey">
         <h2>Prominent Product Approvals<br>For Indian Market</h2>
         <div class="container-fluid row py-5 service-links">
             @if($service)
