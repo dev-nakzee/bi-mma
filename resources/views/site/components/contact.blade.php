6ebd963c569e2bc627660c7bcd4ee2fe2f8cd640
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-10-13 10:44:15 +0530

    commit

--- resources/views/site/components/contact.blade.php
+++ resources/views/site/components/contact.blade.php
@@ -3,8 +3,34 @@
         <img src="{{ asset('/media/codes-1697112570.gif')}}" alt="" class="rounded">
     </div>
     <div class="col-md-6">
+        <h3>Get brochure for free</h3>
         <form>
-
+            <div class="form-row row">
+                <div class="col-md-6">
+                  <input type="text" class="form-control" placeholder="First name">
+                </div>
+                <div class="col-md-6">
+                  <input type="text" class="form-control" placeholder="Last name">
+                </div>
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="Email">
+                </div>
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="Mobile">
+                </div>
+                <div class="col-md-6">
+                    <select class="form-control" placeholder="Select service"></select>
+                </div>
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="Country">
+                </div>
+                <div class="col-md-6">
+                    
+                </div>
+                <div class="col-md-6">
+                    <input type="text" class="form-control" placeholder="captcha">
+                </div>
+            </div>
         </form>
     </div>
 </div>
