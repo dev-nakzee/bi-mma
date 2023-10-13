54c28c7d5d837a62dfdeb803a39e7259062f1786
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-10-13 18:19:24 +0530

    Update main.blade.php

--- resources/views/site/layouts/main.blade.php
+++ resources/views/site/layouts/main.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     @yield('seo')
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta content="index, follow" name="robots"/>
     <meta name="author" content="Rajesh Kumar">
     <meta name="publisher" content="MakeMyApproval"/>
