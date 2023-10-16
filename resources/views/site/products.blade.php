a03cf24033062e920574d433c93c0da39ea76a5a
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-10-16 11:47:56 +0530

    Update products.blade.php

--- resources/views/site/products.blade.php
+++ resources/views/site/products.blade.php
@@ -52,13 +52,7 @@
                 <div class="necessary-certifications section" id="necessary-certifications">
                     <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li class="nav-item" role="presentation">
-                          <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
+                          <button class="nav-link @if($loop->first){{"active"}}@endif" id="{{$service->slug}}" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">{{$service->service}}</button>
                         </li>
                       </ul>
                       <div class="tab-content" id="pills-tabContent">
