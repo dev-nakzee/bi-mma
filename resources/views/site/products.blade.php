5b0a4c965deb41f6d705f04aac7b9f9cfe270930
nakzee <104338457+dev-nakzee@users.noreply.github.com>
2023-10-16 11:36:03 +0530

    Update products.blade.php

--- resources/views/site/products.blade.php
+++ resources/views/site/products.blade.php
@@ -50,27 +50,15 @@
                     <div class="col-md-12">{!! $products->description !!}</div>
                 </div>
                 <div class="necessary-certifications section" id="necessary-certifications">
+                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                     @if($services)
                     @foreach($services as $service)
-                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
                         <li class="nav-item" role="presentation">
-                          <button class="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Home</button>
+                          <button class="nav-link active" id="{{$service->slug}}" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">{{$service->service}}</button>
                         </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Profile</button>
-                        </li>
-                        <li class="nav-item" role="presentation">
-                          <button class="nav-link" id="pills-contact-tab" data-bs-toggle="pill" data-bs-target="#pills-contact" type="button" role="tab" aria-controls="pills-contact" aria-selected="false">Contact</button>
-                        </li>
-                      </ul>
-                      <div class="tab-content" id="pills-tabContent">
-                        <div class="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">...</div>
-                        <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
-                        <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
-                      </div>
-                      
                     @endforeach
                     @endif
+                    </ul>
                 </div>
             </div>
         </div>
